785ebe1d8cf40ca26e4e472c190480c6d7924f35
Estrol <estrol@entrosbot.xyz>
2020-07-17 11:16:52 +0700

    First commit

---
+++ launc.bat
@@ -0,0 +1,11 @@
+@echo off
+cls
+set ipaddress=127.0.0.1
+
+set web=15015
+set game=15010
+set exe=o2interval
+
+echo %exe% 1 %ipaddress% o2jam/patch %ipaddress%:%web% 1 1 1 1 1 1 1 1 %ipaddress% %game% %ipaddress% %game% %ipaddress% %game% %ipaddress% %game% %ipaddress% %game% %ipaddress% %game% %ipaddress% %game% %ipaddress% %game%
+
+%exe% 1 %ipaddress% o2jam/patch %ipaddress%:%web% 1 1 1 1 1 1 1 1 %ipaddress% %game% %ipaddress% %game% %ipaddress% %game% %ipaddress% %game% %ipaddress% %game% %ipaddress% %game% %ipaddress% %game% %ipaddress% %game%
